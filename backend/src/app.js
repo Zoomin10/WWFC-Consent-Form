13c44cda96568dd8ff0dce71448dec0cdf12717d
Zoomin10 <crowe.w@gmail.com>
2026-06-06 13:01:36 +0100

    auth3

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,12 @@ const adminRoutes = require("./routes/admin");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
